eecba85de122843719bf22b94941a6cb6aba7448
Jens Melgaard <admin@it-links.dk>
2020-08-14 11:32:54 +0200

    added another case

--- SonarRepros/SonarRepros/FalsePositiveSwitchNull.cs
+++ SonarRepros/SonarRepros/FalsePositiveSwitchNull.cs
@@ -8,15 +8,15 @@ namespace SonarRepros
     {
         public bool IsEmpty(JToken token)
         {
-           switch (token?.Type)
+            switch (token?.Type)
             {
                 case JTokenType.Array:
                     return !token.HasValues;
                 case JTokenType.Integer:
                 case JTokenType.Float:
-                    return 0 == (int) token;
+                    return 0 == (int)token;
                 case JTokenType.String:
-                    return bool.FalseString.Equals((string) token, StringComparison.OrdinalIgnoreCase);
+                    return bool.FalseString.Equals((string)token, StringComparison.OrdinalIgnoreCase);
                 case JTokenType.Boolean:
                     return !(bool)token;
                 case JTokenType.Null:
@@ -26,6 +26,14 @@ namespace SonarRepros
                 default:
                     return false;
             }
+        }
+
+        public bool NullOrEmpty(JToken token)
+        {
+            return token == null || token.Type == JTokenType.Null ||
+                (token.Type == JTokenType.Array && !token.HasValues) ||
+                (token.Type == JTokenType.Object && !token.HasValues) ||
+                (token.Type == JTokenType.String && string.IsNullOrWhiteSpace((string)token));
         }
 
     }
