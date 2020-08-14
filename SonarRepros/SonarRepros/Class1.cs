574507c6e88a4507d6ce0121e8104aa7f7c15b3b
Jens Melgaard <admin@it-links.dk>
2020-08-14 11:07:59 +0200

    Added repro for second part: https://community.sonarsource.com/t/c-false-positive-variable-is-null-on-at-least-one-execution-path/15121/8

---
+++ SonarRepros/SonarRepros/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonarRepros
+{
+    public enum ValueType
+    {
+        Array, 
+        Integer,
+        String,
+        Null
+    }
+    public class ValueHolder
+    {
+        public ValueType Type { get; set; }
+        public object Value { get; set; }
+    }
+
+    public class FalsePositiveSwitchNull
+    {
+        public bool IsEmpty(ValueHolder holder)
+        {
+            switch (holder?.Type)
+            {
+                case ValueType.Array:
+                    return ((Array) holder.Value).Length == 0;
+                case ValueType.Integer:
+                    return ((int) holder.Value) == 0;
+                case ValueType.String:
+                    return string.IsNullOrWhiteSpace((string) holder.Value);
+                case ValueType.Null:
+                case null:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+    }
+}
